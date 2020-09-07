9d293cd39ce61966a8d07f682cc384bdffa0c6a6
Luiz Antonio Gomes da Silva <luiz.gsilva42@gmail.com>
2020-09-06 23:47:01 -0300

    Navegação

---
+++ src/pages/SignUp/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import {} from 'react-native';
+
+import { Container } from './styled';
+
+const SignUp: React.FC = () =>{
+  return (
+    <Container />
+  );
+}
+
+export default SignUp;
