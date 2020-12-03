4067dc4ef8a8d90c1de77a182aa19ee740c5b8d0
Luiz Silva <luiz.gsilva42@gmail.com>
2020-12-03 09:21:58 -0300

    Criado páginas e rotas

---
+++ src/pages/AppointmentCreated/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View } from 'react-native';
+
+const AppointmentCreated: React.FC = () => {
+  return (
+    <View>
+      Hello AppointmentCreated
+    </View>
+  )
+}
+
+export default AppointmentCreated;
---
+++ src/pages/CreateAppointment/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View } from 'react-native';
+
+const CreateAppointment: React.FC = () => {
+  return (
+    <View>
+      Hello CreateAppointment
+    </View>
+  )
+}
+
+export default CreateAppointment;
